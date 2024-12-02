74f43468947322c2471730d891fbb8b8329b1c70
Nikolaus Spring <54682104+nspring00@users.noreply.github.com>
2024-12-02 20:41:46 +0100

    Improve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,26 +1,18 @@
 import kotlin.math.abs
 
-typealias ParsedInput = Pair<List<Int>, List<Int>>
 
 fun main() {
-    fun parseInput(input: List<String>): ParsedInput {
-        val list1 = mutableListOf<Int>()
-        val list2 = mutableListOf<Int>()
-        for (line in input) {
-            val split = line.split("   ")
-            list1.add(split[0].toInt())
-            list2.add(split[1].toInt())
-        }
-        return Pair(list1, list2)
+    fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> = input.map { line ->
+        val (a, b) = line.split("   ").map { it.toInt() }
+        a to b
+    }.unzip()
+
+    fun part1(input: Pair<List<Int>, List<Int>>): Int {
+        val (list1, list2) = input
+        return list1.sorted().zip(list2.sorted()).sumOf { (n, m) -> abs(n - m) }
     }
 
-    fun part1(input: ParsedInput): Int {
-        val list1 = input.first.sorted()
-        val list2 = input.second.sorted()
-        return list1.zip(list2).sumOf { (n, m) -> abs(n - m) }
-    }
-
-    fun part2(input: ParsedInput): Int {
+    fun part2(input: Pair<List<Int>, List<Int>>): Int {
         val counts = input.second.groupingBy { it }.eachCount()
         return input.first.sumOf { it * (counts[it] ?: 0) }
     }
@@ -29,8 +21,12 @@ fun main() {
     check(part1(testInput) == 11)
     check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = parseInput(readInput("Day01"))
-    part1(input).println()
-    part2(input).println()
+    val result1 = part1(input)
+    val result2 = part2(input)
+    println(result1)
+    println(result2)
+
+    check(result1 == 1151792)
+    check(result2 == 21790168)
 }
